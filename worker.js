50adf3d16973283e29fccac179bb754ca63685ef
anabhidev <anabhi.dev@gmail.com>
2026-07-08 12:59:00 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,13 +3,7 @@ export default {
     const url = new URL(request.url);
 
     if (url.hostname === 'studio.anabhidev.com') {
-      // Buat request baru dengan path ke studio/index.html
-      // menggunakan URL asli tapi ganti pathname
-      const newRequest = new Request(
-        'https://anabhidev.com/studio/index.html',
-        request
-      );
-      return env.ASSETS.fetch(newRequest);
+      return Response.redirect('https://anabhidev.com/studio/', 301);
     }
 
     return env.ASSETS.fetch(request);
