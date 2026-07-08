389d17dda038c07e7dbc8a770087e89d43af6390
anabhidev <anabhi.dev@gmail.com>
2026-07-08 10:44:27 +0700

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,14 +1,13 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    
+
     if (url.hostname === 'studio.anabhidev.com') {
       const studioUrl = new URL(request.url);
-      studioUrl.hostname = url.hostname;
       studioUrl.pathname = '/Studio/index.html';
       return env.ASSETS.fetch(new Request(studioUrl.toString(), request));
     }
-    
+
     return env.ASSETS.fetch(request);
   }
 };
