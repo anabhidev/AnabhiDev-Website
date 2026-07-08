71c1c1e40f953e6681c3004791d06fee1888f512
anabhidev <anabhi.dev@gmail.com>
2026-07-08 12:33:32 +0700

    Add worker script for handling fetch requests

---
+++ worker.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.hostname === 'studio.anabhidev.com') {
+      // Fetch langsung dari workers.dev URL
+      const assetUrl = new URL(request.url);
+      assetUrl.hostname = 'anabhidev-website.anabhi-dev.workers.dev';
+      assetUrl.pathname = '/studio' + (url.pathname === '/' ? '/index.html' : url.pathname);
+      return fetch(assetUrl.toString());
+    }
+
+    return env.ASSETS.fetch(request);
+  }
+};
